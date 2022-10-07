0d754193c9cd202a3544f87783dbccc48efe8781
Windu12345 <32morvantheo@gmail.com>
2022-10-07 15:02:02 +0200

    modifs in main to get confusion matrix

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import seaborn as sns
 from sklearn.preprocessing import OneHotEncoder
 from pipeline_functions import *
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 import optuna
 from xgboost import XGBClassifier
 path = os.path.join("data","PastLoans.csv")
@@ -18,10 +19,11 @@ if __name__ == "__main__":
     X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
     pipeline = full_pipeline()
     X_train_final = pipeline.fit_transform(X_train)
-    X_val_final = pipeline.fit_transform(X_val)
+    X_val_final = pipeline.transform(X_val)
+    X_test_final = pipeline.transform(X_test)
     optuna_objective = create_optuna_pipeline(X_train_final, y_train, X_val_final, y_val)
     study = optuna.create_study(direction="maximize")
-    study.optimize(optuna_objective, n_trials=20, n_jobs=-1)
+    study.optimize(optuna_objective, n_trials=30, n_jobs=-1)
     print("Number of finished trials: ", len(study.trials))
     print("Best trial:")
     trial = study.best_trial
@@ -29,3 +31,10 @@ if __name__ == "__main__":
     print("  Params: ")
     for key, value in trial.params.items():
         print("    {}: {}".format(key, value))
+    model = XGBClassifier(**trial.params)
+    model.fit(X_train_final,y_train)
+    preds = model.predict_proba(X_test_final)
+    preds_class = model.predict(X_test_final)
+    matrix_confusion = confusion_matrix(y_test, preds_class)
+    print(matrix_confusion)
+    ipdb.set_trace()
