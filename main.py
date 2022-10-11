88cb058f8b5056c5a31d7cecbb6eb84e75bddcc7
Antoine Mellerio <antoine.mellerio@orange.fr>
2022-10-11 10:15:07 +0200

    sync

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ import matplotlib.pyplot as plt
 import os
 import seaborn as sns
 from sklearn.preprocessing import OneHotEncoder
+import xgboost
 from pipeline_functions import *
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 import optuna
 from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+import ipdb
+
 
 dirname = os.path.dirname
 root = os.getcwd()
@@ -16,6 +20,10 @@ data_path = os.path.join(dirname(root), 'data')
 path = os.path.join(data_path,"PastLoans.csv")
 path_new_set = os.path.join(data_path,"NewApplications_3_Round1.csv")
 path_output = os.path.join(root + '/data', 'default_predictions.csv')
+
+
+path = os.path.join("data","PastLoans.csv")
+path_new_set = os.path.join("data","NewApplications_3_Round1.csv")
 
 if __name__ == "__main__":
     # Load data
@@ -30,23 +38,54 @@ if __name__ == "__main__":
     X_train_final = pipeline.fit_transform(X_train)
     X_val_final = pipeline.transform(X_val)
     X_test_final = pipeline.transform(X_test)
-
-    # Optimize hyperparameters and train
-    optuna_objective = create_optuna_pipeline_xgboost(X_train_final, y_train, X_val_final, y_val)
+    optuna_objective = create_complete_pipeline(X_train_final, y_train, X_val_final, y_val)
     study = optuna.create_study(direction="maximize")
-    study.optimize(optuna_objective, n_trials=30, n_jobs=-1)
+    study.optimize(optuna_objective, n_trials=5, n_jobs=-1)
     print("Number of finished trials: ", len(study.trials))
     print("Best trial:")
     trial = study.best_trial
     print("  Value: {}".format(trial.value))
+    params_xgb={}
+    params_lgbm={} 
+    params_weights = {}
     print("  Params: ")
     for key, value in trial.params.items():
         print("    {}: {}".format(key, value))
+        if key in liste_lgbm:
+            params_lgbm[key]=value
+        if key in liste_xgb:
+            params_xgb[key]=value
+        else:
+            params_weights[key]=value
 
-    model = XGBClassifier(**trial.params)
-    model.fit(X_train_final,y_train)
-    preds = model.predict_proba(X_test_final)
-    preds_class = model.predict(X_test_final)
+    model_lgbm = LGBMClassifier(**params_lgbm)
+    model_xgb = XGBClassifier(**params_xgb)
+
+    sample_weights = compute_sample_weight(class_weight="balanced",y = y_train)
+    model_xgb.fit(X_train_final,y_train, sample_weight=sample_weights)
+    model_lgbm.fit(X_train_final,y_train, sample_weight=sample_weights)
+
+    preds_lgbm = model_lgbm.predict_proba(X_train_final)[:,1]
+    preds_xgb = model_xgb.predict_proba(X_train_final)[:,1]
+    optuna_logistic = create_logistic_regression_pipeline(preds_lgbm,preds_xgb, y_train)
+    study = optuna.create_study(direction="maximize")
+    study.optimize(optuna_logistic, n_trials=5, n_jobs=-1)
+    print(study.best_trial.params)
+    params_logistic = study.best_trial.params
+    params_logistic["penalty"]="elasticnet"
+    params_logistic["solver"]="saga"
+    params_logistic["class_weight"]="balanced"
+    model_logistic = LogisticRegression(**study.best_trial.params)
+    model_logistic.fit(
+        np.concatenate((preds_lgbm.reshape(-1,1),preds_xgb.reshape(-1,1)),axis=1),
+        y_train
+        )
+
+    preds_lgbm = model_lgbm.predict_proba(X_test_final)[:,1]
+    preds_xgb = model_xgb.predict_proba(X_test_final)[:,1]
+
+    
+    preds_class = optimal_mix_predictions(preds_lgbm,preds_xgb,**params_weights)
     matrix_confusion = confusion_matrix(y_test, preds_class)
 
     print('Confusion matrix:')
@@ -56,11 +95,24 @@ if __name__ == "__main__":
     df_new_preds = pd.read_csv(path_new_set,index_col="id")
     X_new = df_new_preds.iloc[:,:]
     X_new_scaled = pipeline.transform(X_new)
-    predictions = model.predict_proba(X_new_scaled)
+    preds_lgbm = model_lgbm.predict_proba(X_new_scaled)
+    preds_xgb = model_xgb.predict_proba(X_new_scaled)
+
+    
+    final_proba = model_logistic.predict_proba(
+        np.concatenate((preds_lgbm.reshape(-1,1),preds_xgb.reshape(-1,1)),axis=1)
+        )[:,1]
+    
+    predictions = optimal_mix_probas(preds_lgbm,preds_xgb,**params_weights)
     df_preds = pd.DataFrame(predictions, columns=["Proba no Default","Proba Default"])
     df_preds["id"] = df_new_preds.index
 
     df_preds["break_even_rate"] = df_preds["Proba Default"]/(1-df_preds["Proba Default"])
     df_preds["rate"] = df_preds.break_even_rate + 0.02
+    fig, ax =plt.subplots(1,1,figsize=(12,10))
+    sns.distplot(final_proba)
+    plt.show()
+    # ipdb.set_trace()
+    # df_preds[['id', 'rate']].to_csv(path_output, header=True, index=False)
 
-    df_preds[['id', 'rate']].to_csv(path_output, header=True, index=False)
+    
--- pipeline_functions.py
+++ pipeline_functions.py
@@ -1,4 +1,5 @@
 from pyclbr import Function
+from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler, MinMaxScaler,FunctionTransformer
 from sklearn.utils.class_weight import compute_sample_weight
@@ -13,6 +14,7 @@ import optuna
 from xgboost import XGBClassifier
 from lightgbm import LGBMClassifier
 import ipdb
+from sklearn.ensemble import HistGradientBoostingRegressor, RandomForestRegressor, StackingRegressor
 
 def _log_transform_income(X):
     X = X.copy()
@@ -98,3 +100,118 @@ def create_optuna_pipeline_lightgbm(X_train,y_train, X_val, y_val):
         return final_score
 
     return optuna_objective
+
+def optimal_mix_predictions(preds_1,preds_2,**kwargs):
+    if "weight" in list(kwargs.keys()):
+        weight = kwargs["weight"]
+        value = weight*preds_1 + (1-weight)*preds_2
+    else:
+        weight = 1/2
+        value = weight*preds_1 + (1-weight)*preds_2
+    
+    if "cutting_threshold" in list(kwargs.keys()):
+        final_class = (value>=kwargs["cutting_threshold"])*1
+    else:
+        final_class = (value>=0.5)*1
+    return final_class
+
+def optimal_mix_probas(preds_1,preds_2,**kwargs):
+    if "weight" in list(kwargs.keys()):
+        weight = kwargs["weight"]
+        value = weight*preds_1 + (1-weight)*preds_2
+    else:
+        weight = 1/2
+        value = weight*preds_1 + (1-weight)*preds_2
+    return value
+
+def create_complete_pipeline(X_train,y_train, X_val, y_val):
+    def optuna_objective(trial):
+        params_lgbm = {
+        "objective": "binary",
+        "metric": "binary_logloss",
+        # "verbosity": -1,
+        "boosting_type": "gbdt",
+        "lambda_l1": trial.suggest_float("lambda_l1", 1e-8, 10.0, log=True),
+        "lambda_l2": trial.suggest_float("lambda_l2", 1e-8, 10.0, log=True),
+        "num_leaves": trial.suggest_int("num_leaves", 2, 256),
+        "feature_fraction": trial.suggest_float("feature_fraction", 0.4, 1.0),
+        "bagging_fraction": trial.suggest_float("bagging_fraction", 0.4, 1.0),
+        "bagging_freq": trial.suggest_int("bagging_freq", 1, 7),
+        "min_child_samples": trial.suggest_int("min_child_samples", 5, 100),
+        "learning_rate": trial.suggest_float("learning_rate",1e-5,1e-1,log=True)
+        }
+        params_xgb =  {
+            # L2 regularization weight.
+            "lambda": trial.suggest_float("lambda", 1e-8, 1.0, log=True),
+            # L1 regularization weight.
+            "alpha": trial.suggest_float("alpha", 1e-8, 1.0, log=True),
+            # sampling ratio for training data.
+            "subsample": trial.suggest_float("subsample", 0.2, 1.0),
+            # sampling according to each tree.
+            "colsample_bytree": trial.suggest_float("colsample_bytree", 0.2, 1.0),
+            "max_depth": trial.suggest_int("max_depth", 3, 9, step=2),
+            "min_child_weight" : trial.suggest_int("min_child_weight", 2, 10),
+            "n_estimators": trial.suggest_int("n_estimators",50,150, step=25)
+        }
+        sample_weights = compute_sample_weight(class_weight="balanced",y = y_train)
+        model_lgbm = LGBMClassifier(**params_lgbm)
+        model_xgb = XGBClassifier(**params_xgb)
+        estimators = [
+            ("lgbm", model_lgbm),
+            ("xgboost", model_xgb)
+        ]
+        model_lgbm.fit(X_train, y_train, sample_weight=sample_weights)
+        model_xgb.fit(X_train, y_train, sample_weight=sample_weights)
+        preds_lgbm = model_lgbm.predict_proba(X_val)[:,1]
+        preds_xgb = model_xgb.predict_proba(X_val)[:,1]
+        # ipdb.set_trace()
+        params_cuts = {
+            "weight": trial.suggest_float("weight",0,1),
+            # "cutting_threshold":trial.suggest_float("cutting_threshold",0.,1)
+        }
+        preds = optimal_mix_predictions(preds_lgbm,preds_xgb,**params_cuts)
+        # ipdb.set_trace()
+        final_score = recall_score(y_val, preds)
+
+        # final_model = StackingRegressor(estimators=estimators, final_estimator=HistGradientBoostingRegressor())
+        return final_score
+    
+    return optuna_objective
+
+def create_logistic_regression_pipeline(preds_1,preds_2,y_true):
+
+    def optuna_objective(trial):
+        params = {
+            "l1_ratio":trial.suggest_float("l1_ratio",0,1),
+            "penalty":"elasticnet",
+            "solver":"saga",
+            "class_weight":"balanced",
+        }
+        model = LogisticRegression(**params)
+        X = np.concatenate((preds_1.reshape(-1,1),preds_2.reshape(-1,1)),axis=1)
+        model.fit(X, y_true)
+
+        preds = model.predict(X)
+        score = f1_score(y_true,preds)
+        return score
+    return optuna_objective
+
+liste_lgbm = ["lambda_l1",
+    "lambda_l2",
+    "num_leaves",
+    "feature_fraction",
+    "bagging_fraction",
+    "bagging_freq",
+    "min_child_samples",
+    'learning_rate'
+]
+liste_xgb = [
+    "lambda",
+    "alpha",
+    "subsample",
+    "colsample_bytree",
+    "max_depth",
+    "min_child_weight",
+    "n_estimators"
+]
+liste_weights = ["weight","cutting_threshold"]
