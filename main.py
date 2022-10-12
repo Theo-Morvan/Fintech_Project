2215a6484820b45719ba154b2402fa8725ee3777
Windu12345 <32morvantheo@gmail.com>
2022-10-12 11:33:26 +0200

    cleaning main and adapting to mix model

--- main.py
+++ main.py
@@ -20,13 +20,12 @@ root = os.getcwd()
 data_path = os.path.join(dirname(root), 'data')
 path = os.path.join(data_path,"PastLoans.csv")
 path_new_set = os.path.join(data_path,"NewApplications_3_Round1.csv")
-path_output = os.path.join(root + '/data', 'default_predictions_backtesting_logistic.csv')
-
+path_output = os.path.join(root + '/data', 'default_predictions_backtesting_mix_models.csv')
 
 path = os.path.join("data","PastLoans.csv")
 path_new_set = os.path.join("data","NewApplications_3_Round1.csv")
 
-save = False
+save = True
 
 if __name__ == "__main__":
     # Load data
@@ -60,61 +59,54 @@ if __name__ == "__main__":
             params_xgb[key]=value
         else:
             params_weights[key]=value
-
+    print(params_weights)
     model_lgbm = LGBMClassifier(**params_lgbm)
     model_xgb = XGBClassifier(**params_xgb)
+    params_logistic = dict()
+    params_logistic["penalty"]='elasticnet'
+    params_logistic["solver"]="saga"
+    params_logistic["class_weight"]="balanced"
+    model_logistic = LogisticRegression(penalty="elasticnet",solver="saga",class_weight="balanced",l1_ratio=0.5)
 
     sample_weights = compute_sample_weight(class_weight="balanced",y = y_train)
     model_xgb.fit(X_train_final,y_train, sample_weight=sample_weights)
     model_lgbm.fit(X_train_final,y_train, sample_weight=sample_weights)
+    model_logistic.fit(X_train_final,y_train, sample_weight=sample_weights)
 
-    preds_lgbm = model_lgbm.predict_proba(X_train_final)[:,1]
-    preds_xgb = model_xgb.predict_proba(X_train_final)[:,1]
-    optuna_logistic = create_logistic_regression_pipeline(preds_lgbm,preds_xgb, y_train)
-    study = optuna.create_study(direction="maximize")
-    study.optimize(optuna_logistic, n_trials=5, n_jobs=-1)
-    print(study.best_trial.params)
-    params_logistic = study.best_trial.params
-    params_logistic["penalty"]="elasticnet"
-    params_logistic["solver"]="saga"
-    params_logistic["class_weight"]="balanced"
-    model_logistic = LogisticRegression(**study.best_trial.params)
-    model_logistic.fit(
-        np.concatenate((preds_lgbm.reshape(-1,1),preds_xgb.reshape(-1,1)),axis=1),
-        y_train
-    )
     # ipdb.set_trace()
     preds_lgbm = model_lgbm.predict_proba(X_test_final)[:,1]
     preds_xgb = model_xgb.predict_proba(X_test_final)[:,1]
-    preds_class = optimal_mix_predictions(preds_lgbm,preds_xgb,**params_weights)
-    logisic_predictions= model_logistic.predict(np.concatenate((preds_lgbm.reshape(-1,1),preds_xgb.reshape(-1,1)),axis=1))
+    preds_logistic = model_logistic.predict_proba(X_test_final)[:,1]
+    preds_mix = optimal_mix_probas(preds_lgbm,preds_xgb,preds_logistic,False,**params_weights)
+    preds_class = (0.5<=preds_mix)*1
     matrix_confusion = confusion_matrix(y_test, preds_class)
 
 
     print('Confusion matrix:')
     print(matrix_confusion)
 
-    print(f"f1_score is with mix XGB/LGBM: {f1_score(y_test,preds_class)}")
+    print(f"f1_score is with mix XGB/LGBM/LogisticRegression: {f1_score(y_test,preds_class)}")
     print(f"f1_score with logisitic regression upon models : {f1_score(y_test, preds_class)}")
-
+    
+    
     # Predict
     df_new_preds = pd.read_csv(path_new_set,index_col="id")
     X_new = df_new_preds.iloc[:,:]
     X_new_scaled = pipeline.transform(X_new)
     preds_lgbm = model_lgbm.predict_proba(X_new_scaled)
     preds_xgb = model_xgb.predict_proba(X_new_scaled)
+    preds_logistic = model_logistic.predict_proba(X_new_scaled)
 
     
-    final_proba = model_logistic.predict_proba(
-        np.concatenate((preds_lgbm[:,1].reshape(-1,1),preds_xgb[:,1].reshape(-1,1)),axis=1)
-        )
-
-    predictions = optimal_mix_probas(preds_lgbm,preds_xgb,**params_weights)
+    # final_proba = model_logistic.predict_proba(
+    #     np.concatenate((preds_lgbm[:,1].reshape(-1,1),preds_xgb[:,1].reshape(-1,1)),axis=1)
+    #     )
+    predictions = optimal_mix_probas(preds_lgbm,preds_xgb,preds_logistic,**params_weights)
     y_preds = predictions.argmax(axis=1)
-    df_preds = pd.DataFrame(final_proba, columns=["Proba no Default","Proba Default"])
-    ipdb.set_trace()
+    df_preds = pd.DataFrame(predictions, columns=["Proba no Default","Proba Default"])
     if save :
         df_preds.to_csv(path_output, header=True, index=False)
+    ipdb.set_trace()
     # ipdb.set_trace()
     # df_preds["id"] = df_new_preds.index
     # ipdb.set_trace()
